a3554bad26a442234350b981d91ba1cbf86e4dbe
John Botella <john.botella@atm-consulting.fr>
2020-09-03 10:19:11 +0200

    Add getnom url infos

--- class/actions_invoiceslate.class.php
+++ class/actions_invoiceslate.class.php
@@ -67,19 +67,81 @@ class ActionsInvoiceslate
 
 
 	/**
-	 * Execute action
+	 * Affichage en rouge et gras du nom du tiers si impayé en retard
 	 *
 	 * @param	array			$parameters		Array of parameters
 	 * @param	CommonObject    $object         The object to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
 	 * @param	string			$action      	'add', 'update', 'view'
+	 *
 	 * @return	int         					<0 if KO,
 	 *                           				=0 if OK but we want to process standard actions too,
 	 *                            				>0 if OK and we want to replace standard actions.
 	 */
-	public function getNomUrl($parameters, &$object, &$action)
+	function getNomUrl($parameters, &$object, &$action, $hookmanager)
 	{
-		global $db, $langs, $conf, $user;
-		$this->resprints = '';
+		if($object->element == 'societe') {
+			global $langs;
+			$langs->load('bills');
+			$langs->load('invoiceslate@invoiceslate');
+			$dataClient = $this->_getDataClient($object->id);
+			if($dataClient) {
+
+				$dom = new DOMDocument();
+				@$dom->loadHTML (mb_convert_encoding($parameters['getnomurl'], 'HTML-ENTITIES', "UTF-8"));
+				$links = $dom->getElementsByTagName ( 'a');
+
+				foreach ($links as $link){
+
+					$title = $link->getAttribute('title');
+					$class = $link->getAttribute('class');
+
+					// Unpaid
+					if($dataClient->total_unpaid>0){
+						$style = $link->getAttribute('style');
+						$style.= 'color: red; font-weight: bold;';
+						$link->setAttribute('style', $style);
+
+						$title.= '<div style="color:red; font-weight: bold;">'. $langs->trans('Unpaid').' : '.price($dataClient->total_unpaid).'</div>';
+					}
+
+					// last Order
+					if($dataClient->last_order){
+						$title.= '<div>'. $langs->trans('LastOrder').' : '.dol_print_date($dataClient->last_order).'</div>';
+					}
+
+					// Have a tooltip ?
+					$haveToolTip = strpos($class, 'classfortooltip');
+					if($haveToolTip !== false){
+						$link->setAttribute('title', $title);
+					}
+				}
+
+				$this->resprints = $dom->saveHTML();
+
+				// last Order badge
+				if($dataClient->last_order){
+
+					$now = new DateTime();
+					$last_order = new DateTime();
+					$last_order->setTimestamp($dataClient->last_order);
+					$interval = $now->diff($last_order);
+					$nbMonth = intval($interval->format('%r%m'));
+					$badgeClass = "badge-primary";
+					if($nbMonth<6){
+						$badgeClass = "badge-warning";
+					}
+					elseif($nbMonth<3){
+						$badgeClass = "badge-info";
+					}
+
+					$this->resprints.= '<span class="classfortooltip badge '.$badgeClass.'" title="'.dol_htmlentities($title, ENT_QUOTES).'" >'.$nbMonth.'</span>';
+				}
+
+
+				return 1;
+			}
+		}
+
 		return 0;
 	}
 
@@ -129,4 +191,61 @@ class ActionsInvoiceslate
 
 	}
 
+
+
+
+	/**
+	 * Récupération des données qui permettent de définir si le tiers est à afficher en rouge
+	 * - Facture impayée dont la date d'échénace et dépasée
+	 * - Top 10 client TODO ?
+	 *
+	 *
+	 * @return boolean | object      false or objet of data
+	 */
+	function _getDataClient($fk_soc = 0, $useCache = true) {
+		global $db, $INVOICESLATE_CACHE_getDataClient;
+
+		$fk_soc = intval($fk_soc);
+
+		if($useCache && isset($INVOICESLATE_CACHE_getDataClient[$fk_soc])) return $INVOICESLATE_CACHE_getDataClient[$fk_soc];
+
+		// init cache
+		if(!isset($INVOICESLATE_CACHE_getDataClient)){
+			$INVOICESLATE_CACHE_getDataClient = array();
+		}
+
+		$customerData = new stdClass();
+		$customerData->errorMsg = array();
+		$customerData->errors = 0;
+
+		// Get unpaid
+		$customerData->total_unpaid = 0;
+		$sql = 'SELECT SUM(total_ttc) as total_unpaid FROM '.MAIN_DB_PREFIX.'facture WHERE fk_statut =1 AND paye = 0 AND date_lim_reglement < NOW() AND fk_soc = '.intval($fk_soc);
+		$resql = $db->query($sql);
+		if($resql){
+			$obj = $db->fetch_object($resql);
+			$customerData->total_unpaid = $obj->total_unpaid;
+		}
+		else{
+			$customerData->errors ++;
+			$customerData->errorMsg[] = $db->error();
+		}
+
+		// Last order
+		$customerData->last_order = false;
+		$sql = 'SELECT max(date_valid) as last_order FROM '.MAIN_DB_PREFIX.'commande WHERE fk_statut >= 1 AND fk_soc = '.intval($fk_soc);
+		$resql = $db->query($sql);
+		if($resql){
+			$obj = $db->fetch_object($resql);
+			$customerData->last_order = $db->jdate($obj->last_order);
+		}
+		else{
+			$customerData->errors ++;
+			$customerData->errorMsg[] = $db->error();
+		}
+
+
+		$INVOICESLATE_CACHE_getDataClient[$fk_soc] = $customerData;
+		return $customerData;
+	}
 }
